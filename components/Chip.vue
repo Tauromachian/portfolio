4ef3c007fa6d637b21c5ba7cca7748890edc42fa
Jose Garcia <jogarcia@tuta.io>
2021-05-15 16:46:17 -0400

    Add classes

--- components/Chip.vue
+++ components/Chip.vue
@@ -1,5 +1,11 @@
 <template>
-  <span class="rounded-lg p-2 bg-gray-200">{{ text }}</span>
+  <button
+    class="rounded-lg p-2 bg-gray-200"
+    :class="buttonClasses"
+    @click="isActive = !isActive"
+  >
+    {{ text }}
+  </button>
 </template>
 
 <script>
@@ -9,6 +15,31 @@ export default {
     text: {
       type: String,
       required: true
+    },
+    value: {
+      type: Boolean,
+      default: false
+    },
+    isActivable: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    isActive: {
+      get () {
+        return this.value
+      },
+      set (val) {
+        this.$emit('input', val)
+      }
+    },
+    buttonClasses () {
+      if (!this.isActivable) { return {} }
+      return {
+        'bg-green-400': this.isActive,
+        'hover:bg-green-500': this.isActive
+      }
     }
   }
 }
