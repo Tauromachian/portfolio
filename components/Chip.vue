98e752cf4d47bb6654a9661a3391cad16e8099e9
Jose Garcia <jogarcia@tuta.io>
2021-05-15 14:44:41 -0400

    Create basic Chip component

---
+++ components/Chip.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="rounded-lg p-2 bg-gray-200">{{ text }}</span>
+</template>
+
+<script>
+export default {
+  name: 'Chip',
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+span {
+  height: min-content;
+}
+</style>
