6f7335b430c937cb30fe0c1ad033c5f2fc9e0247
Jose Garcia <jogarcia@tuta.io>
2021-05-15 19:31:06 -0400

    Create BaseSection component

---
+++ components/BaseSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-5 mb-5">
+    <h1 class="mb-3 text-2xl md:text-4xl">
+      {{ title }}
+    </h1>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSection',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
