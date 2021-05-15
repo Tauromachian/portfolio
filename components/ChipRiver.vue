2cdc5f07108a38be91d84491bfe86e3ecbbb851f
Jose Garcia <jogarcia@tuta.io>
2021-05-15 15:01:59 -0400

    Create ChipRiver component

---
+++ components/ChipRiver.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex-wrapp">
+    <Chip v-for="(value, index) in values" :key="technology + index" :text="value" />
+  </div>
+</template>
+
+<script>
+import Chip from '~/components/Chip'
+
+export default {
+  name: 'ChipRiver',
+  components: {
+    Chip
+  },
+  props: {
+    values: {
+      type: [Array, Object],
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
