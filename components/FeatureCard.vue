f4c0c84d6ffbadaa1bddce9840570b1e2ef3c423
Jose Garcia <jogarcia@tuta.io>
2021-05-22 14:56:41 -0400

    Add FeatureCard component

---
+++ components/FeatureCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex">
+    <span class="mdi text-7xl mr-1.5 text-green-400" :class="icon" />
+    <div class="flex flex-col">
+      <h2 class="bold">
+        {{ title }}
+      </h2>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeatureCard',
+  props: {
+    title: {
+      type: String,
+      default: 'Lorem'
+    },
+    icon: {
+      type: String,
+      default: 'mdi-emoticon'
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
