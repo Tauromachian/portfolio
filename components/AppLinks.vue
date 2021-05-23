20f1a01f1c01566c5a02293a135bfe5a864b6d9d
Jose Garcia <jogarcia@tuta.io>
2021-05-23 15:34:51 -0400

    Make basic links component

---
+++ components/AppLinks.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <li
+      v-for="(link, index) in links"
+      :key="link.route + index"
+      :class="{ inline: horizontal }"
+    >
+      {{ link.text }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'AppLinks',
+  props: {
+    horizontal: {
+      type: [Boolean, String],
+      default: false
+    }
+  },
+  data () {
+    return {
+      links: [
+        { route: '#', text: 'Sobre mi' },
+        { route: '#', text: 'Portafolio' },
+        { route: '#', text: 'Redes sociales' },
+        { route: '#', text: 'Repositorios' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.inline {
+  display: inline;
+}
+</style>
