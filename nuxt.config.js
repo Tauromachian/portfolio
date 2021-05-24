b93ec05353c58820b2991c2dbd82a1bea9bc00e4
Jose Garcia <jogarcia@tuta.io>
2021-05-23 22:51:52 -0400

    Add i18n to modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,12 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    [
+      'nuxt-i18n',
+      { /* module options */ }
+    ]
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
