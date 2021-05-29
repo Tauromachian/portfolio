0bce4642737266841ae4ab99f79bf9455e5b15f5
Jose Garcia <jogarcia@tuta.io>
2021-05-29 11:03:51 -0400

    Create basic BottomNav navigation component

---
+++ components/BottomNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex justify-center bg-primary px-3">
+    <base-button icon>
+      <span class="mdi mdi-translate text-xl" />
+    </base-button>
+    <base-button icon>
+      <span class="mdi mdi-invert-colors text-xl" />
+    </base-button>
+    <base-button icon>
+      <span class="mdi mdi-menu text-xl" />
+    </base-button>
+  </div>
+</template>
+
+<script>
+// import AppLinks from './AppLinks'
+import BaseButton from './BaseButton'
+
+export default {
+  name: 'BottomNav',
+  components: {
+    BaseButton
+    // AppLinks
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+app-links li {
+  display: inline;
+}
+</style>
