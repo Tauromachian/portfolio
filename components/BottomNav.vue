f52ff267c8e16a262d7df645d8b69cd80f11b930
Jose Garcia <jogarcia@tuta.io>
2021-05-30 20:30:45 -0400

    Make the links an accordion

--- components/BottomNav.vue
+++ components/BottomNav.vue
@@ -1,14 +1,35 @@
 <template>
-  <div class="flex justify-center bg-primary px-3 text-2xl">
-    <base-button icon @click="changeLocal">
-      <span class="mdi mdi-translate" />
-    </base-button>
-    <base-button icon @click="changeTheme">
-      <span class="mdi mdi-invert-colors" />
-    </base-button>
-    <base-button icon>
-      <span class="mdi mdi-menu" />
-    </base-button>
+  <div class="flex flex-col">
+    <div
+      class="flex flex-col items-center justify-center text-center bg-primary"
+      :class="{ hidden: isHidden }"
+    >
+      <ul>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#social-networks">Social Networks</a>
+        </li>
+        <li>
+          <a href="#repositories">Repositories</a>
+        </li>
+      </ul>
+    </div>
+    <div class="flex justify-center bg-primary px-3 text-2xl">
+      <base-button icon @click="changeLocal">
+        <span class="mdi mdi-translate" />
+      </base-button>
+      <base-button icon @click="changeTheme">
+        <span class="mdi mdi-invert-colors" />
+      </base-button>
+      <base-button icon @click="isHidden = !isHidden">
+        <span class="mdi mdi-menu" />
+      </base-button>
+    </div>
   </div>
 </template>
 
@@ -21,6 +42,11 @@ export default {
   components: {
     BaseButton
     // AppLinks
+  },
+  data () {
+    return {
+      isHidden: false
+    }
   },
   methods: {
     changeLocal () {
