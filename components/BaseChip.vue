8638eee92de5c94a3fb57eca6fe0d13869755087
Jose Garcia <jogarcia@tuta.io>
2021-06-06 10:48:31 -0400

    Create BaseChip component

---
+++ components/BaseChip.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="rounded-lg p-2"
+    @click="$emit('click')"
+  >
+    <slot>
+      {{ text }}
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
