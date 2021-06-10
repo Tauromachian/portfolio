67a037143266ee536a41d8e8df44a8c17201de4d
Jose Garcia <jogarcia@tuta.io>
2021-06-09 21:02:26 -0400

    Set links

--- store/index.js
+++ store/index.js
@@ -1,6 +1,12 @@
 export const state = () => ({
   theme: 'default',
-  themes: ['default', 'crazy']
+  themes: ['default', 'crazy'],
+  links: [
+    { link: '#about', text: 'About' },
+    { link: '#portfolio', text: 'Portfolio' },
+    { link: '#social-networks', text: 'Repositories' },
+    { link: '#repositories', text: 'Repositories' }
+  ]
 })
 
 export const mutations = {
