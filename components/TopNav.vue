08c06209f9e2c0fa030bf114d9e17ee5c9674106
Jose Garcia <jogarcia@tuta.io>
2021-06-09 21:47:49 -0400

    Smoothly scroll to element

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -9,7 +9,7 @@
       </base-button>
       <ul class="flex items-center text-xl ">
         <li v-for="link in links" :key="link.link + link.text" class="px-3 navbar__link">
-          <a :href="link.link">
+          <a :href="link.link" @click.prevent="scrollToPosition(link.link)">
             {{ link.text }}
           </a>
         </li>
@@ -52,6 +52,10 @@ export default {
         this.setTheme('crazy')
         document.documentElement.setAttribute('theme', 'crazy')
       }
+    },
+    scrollToPosition (postitionId) {
+      const elmnt = document.getElementById(postitionId.split('#').pop())
+      elmnt.scrollIntoView({ behavior: 'smooth' })
     }
   }
 }
