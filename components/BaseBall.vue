44bd57b01f3c8b4434da839c68d0cddb640bf65e
Jose Garcia <jogarcia@tuta.io>
2021-06-11 09:36:49 -0400

    Create baseBall component

---
+++ components/BaseBall.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="rounded-full ball"
+    :style="{ 'z-index': zIndex, top, bottom, left, right }"
+    :class="{ small, medium, big }"
+  />
+</template>
+
+<script>
+export default {
+  name: 'BaseBall',
+  props: {
+    zIndex: {
+      type: Number,
+      default: 0
+    },
+    big: {
+      type: Boolean,
+      default: false
+    },
+    medium: {
+      type: Boolean,
+      default: true
+    },
+    small: {
+      type: Boolean,
+      default: false
+    },
+    top: {
+      type: String,
+      default: 'initial'
+    },
+    bottom: {
+      type: String,
+      default: 'initial'
+    },
+    left: {
+      type: String,
+      default: 'initial'
+    },
+    right: {
+      type: String,
+      default: 'initial'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ball {
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
+  position: absolute;
+}
+
+.big {
+  height: 150px;
+  width: 150px;
+}
+.medium {
+  height: 100px;
+  width: 100px;
+}
+.small {
+  height: 50px;
+  width: 50px;
+}
+</style>
