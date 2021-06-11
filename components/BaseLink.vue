1e50c74259a538a3609ceecb920c7f56993cb94f
Jose Garcia <jogarcia@tuta.io>
2021-06-11 19:41:45 -0400

    Create basic BaseLink component

---
+++ components/BaseLink.vue
@@ -0,0 +1,28 @@
+<template>
+  <a
+    class="mt-1 px-2"
+  >
+    <span v-if="icon" class="mdi mdi-link" />
+    {{ text }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'BaseLink',
+  props: {
+    icon: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      default: 'lorem ipsum'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
