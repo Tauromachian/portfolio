8ea4565d2233db631d830165382e09757a8d9738
Jose Garcia <jogarcia@tuta.io>
2021-06-11 20:42:33 -0400

    Add small hr animation

--- components/BaseLink.vue
+++ components/BaseLink.vue
@@ -1,9 +1,10 @@
 <template>
-  <a
-    class="mt-1 px-2"
-  >
-    <span v-if="icon" class="mdi mdi-link" />
-    {{ text }}
+  <a class="mt-1 px-2">
+    <span class="link">
+      <span v-if="icon" class="mdi mdi-link" />
+      {{ text }}
+      <hr>
+    </span>
   </a>
 </template>
 
@@ -24,5 +25,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.link {
+  transition: all linear 1s;
+  width: auto;
+  display: inline-block;
+}
 
+hr {
+  transition: width linear 0.2s;
+  border-color: var(--text-color-primary);
+  width: 0%;
+}
+
+.link:hover > hr {
+  width: 100%;
+}
 </style>
