1e09b40c4e886a798185a749914242fb5d1245da
Jose Garcia <jogarcia@tuta.io>
2021-06-12 20:58:57 -0400

    Make basic BaseInputText component

---
+++ components/BaseInputText.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex" :class="{ 'flex-col': col }">
+    <label>{{ label }}</label>
+    <input
+      class="p-1"
+      :type="type"
+      :placeholder="placeholder"
+      v-bind="$attrs"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInputText',
+  props: {
+    label: {
+      type: String,
+      default: 'input'
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    col: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
