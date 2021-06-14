5ecb9ed757b33ab19673ac9fab2406f5c7ea35a4
Jose Garcia <jogarcia@tuta.io>
2021-06-14 16:36:41 -0400

    Create basic BaseTextArea component

---
+++ components/BaseAreaText.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex" :class="{ 'flex-col': col }">
+    <label>{{ label }}</label>
+    <textarea
+      class="p-1 rounded"
+      cols="10"
+      rows="4"
+      placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseTextArea',
+  props: {
+    label: {
+      type: String,
+      default: 'input'
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    col: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  color: var(--text-color-secondary)
+}
+</style>
