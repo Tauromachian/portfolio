02bfac435ef79d39cf70cc265f56da0456f72019
Jose Garcia <jogarcia@tuta.io>
2021-06-16 20:00:19 -0400

    Make basic CardProject component

---
+++ components/CardProject.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-card>
+    <img :src="src" class="shadow max-w-full h-auto align-middle border-none">
+    <div class="flex flex-col">
+      <h2 class="font-bold">
+        {{ title }}
+      </h2>
+      <p>
+        {{ description }}
+      </p>
+    </div>
+    <base-button>Visit site</base-button>
+  </base-card>
+</template>
+
+<script>
+export default {
+  name: 'CardPortfolio',
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    siteUrl: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
