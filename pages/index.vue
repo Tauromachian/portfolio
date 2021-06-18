83cf58beb45ff6d10a850e9eda57f2499ff1e238
Jose Garcia <jogarcia@tuta.io>
2021-06-17 20:51:05 -0400

    Make projects align one next to the other

--- pages/index.vue
+++ pages/index.vue
@@ -59,10 +59,11 @@
       class="flex flex-col items-center justify-center"
       :title="$t('projects')"
     >
-      <div class="flex flex-col">
+      <div class="flex flex-col lg:flex-row ">
         <card-project
           v-for="(project, index) in projects"
           :key="project.src + index"
+          class="md:mx-3"
           :title="project.title"
           :description="project.description"
           :site-url="project.link"
