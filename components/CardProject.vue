b51bac2387f25f8817eebe715100eb8e5cdf47c5
Jose Garcia <jogarcia@tuta.io>
2021-06-17 21:12:18 -0400

    Set max height if screen is big and hide the overflow

--- components/CardProject.vue
+++ components/CardProject.vue
@@ -5,7 +5,7 @@
       <h2 class="font-bold">
         {{ title }}
       </h2>
-      <p class="h-100px overflow-hidden">
+      <p class="lg:h-24 overflow-hidden">
         {{ description }}
       </p>
       <a :href="siteUrl">
