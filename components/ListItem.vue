0e1051be3d4812a0a75fd49dd265cddd1f5faa00
Jose Garcia <jogarcia@tuta.io>
2021-06-19 17:25:22 -0400

    Create ListItem component

---
+++ components/ListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="list-item self-center align-left" @click="click">
+    <slot class="text-base" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  methods: {
+    click () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.list-item {
+  cursor: pointer;
+}
+</style>
