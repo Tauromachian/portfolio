b44764b4aab5e168408932e6ee57f19d42df6ebf
Jose Garcia <jogarcia@tuta.io>
2021-06-20 15:43:22 -0400

    Show active theme

--- components/ThemeSelector.vue
+++ components/ThemeSelector.vue
@@ -1,8 +1,9 @@
 <template>
   <list-wrapper>
     <list-item
-      v-for="(theme, index) in themes"
+      v-for="(theme, index) in activeThemes"
       :key="theme.value + index"
+      :active="theme.active"
       @click="setTheme(theme.value)"
     >
       {{ theme.text }}
@@ -23,6 +24,12 @@ export default {
     }
   },
   computed: {
+    activeThemes () {
+      return this.themes.map((theme) => {
+        theme.active = (theme.value === this.selectedTheme)
+        return theme
+      })
+    },
     selectedTheme: {
       get () {
         return this.value
