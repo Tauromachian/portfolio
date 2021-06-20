158f4ad89682cfa0e2c8fe3105e08f9b1fb7c702
Jose Garcia <jogarcia@tuta.io>
2021-06-20 11:46:13 -0400

    Use menu for the themes

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -4,9 +4,14 @@
       <base-button class="navbar__link" icon @click="changeLocal">
         <span class="mdi mdi-translate" />
       </base-button>
-      <base-button class="navbar__link" icon @click="changeTheme">
-        <span class="mdi mdi-invert-colors" />
-      </base-button>
+      <base-menu tabindex="0" bottom="initial" top="3rem">
+        <template #activator="{ on }">
+          <base-button class="navbar__link" icon v-on="on">
+            <span class="mdi mdi-invert-colors" />
+          </base-button>
+        </template>
+        <theme-selector v-model="theme" :themes="themes" />
+      </base-menu>
       <ul class="flex items-center text-xl ">
         <li v-for="link in links" :key="link.link + link.text" class="px-3 navbar__link">
           <a :href="link.link" @click.prevent="scrollToPosition(link.link)">
@@ -29,11 +34,17 @@ export default {
   },
   data () {
     return {
-      isHidden: false
+      isHidden: false,
+      theme: 'default'
     }
   },
   computed: {
-    ...mapState(['theme', 'links'])
+    ...mapState(['links', 'themes'])
+  },
+  watch: {
+    theme (val) {
+      this.setTheme(val)
+    }
   },
   methods: {
     ...mapMutations(['setTheme']),
@@ -41,15 +52,6 @@ export default {
       this.$i18n.locale === 'en'
         ? this.$i18n.setLocale('es')
         : this.$i18n.setLocale('en')
-    },
-    changeTheme () {
-      if (document.documentElement.hasAttribute('theme')) {
-        this.setTheme('default')
-        document.documentElement.removeAttribute('theme')
-      } else {
-        this.setTheme('crazy')
-        document.documentElement.setAttribute('theme', 'crazy')
-      }
     },
     scrollToPosition (postitionId) {
       const elmnt = document.getElementById(postitionId.split('#').pop())
