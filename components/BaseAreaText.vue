f9f07183dec1b7d84807266d07c79fd8d766f52e
Jose Garcia <jogarcia@tuta.io>
2021-06-21 20:14:43 -0400

    Add new styles for area and text

--- components/BaseAreaText.vue
+++ components/BaseAreaText.vue
@@ -2,7 +2,7 @@
   <div class="flex" :class="{ 'flex-col': col }">
     <label>{{ label }}</label>
     <textarea
-      class="p-1 rounded shadow-lg no-outline"
+      class="p-1 rounded no-outline textarea"
       cols="10"
       rows="4"
       placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
@@ -35,7 +35,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-textarea {
-  color: var(--text-color-gray-900)
+.textarea {
+  color: var(--text-color-gray-900);
+  outline : none;
+  transition: all 0.1s;
+  --shadow-color: #9ca3af ;
+  box-shadow: 6px 6px 0px 1px var(--shadow-color);
+}
+
+.textarea:focus {
+  transform: translate(2px, 2px);
+  box-shadow: 4px 4px 0px 1px var(--shadow-color);
 }
 </style>
--- components/BaseInputText.vue
+++ components/BaseInputText.vue
@@ -2,7 +2,7 @@
   <div class="flex" :class="{ 'flex-col': col }">
     <label>{{ label }}</label>
     <input
-      class="p-1 rounded shadow-lg no-outline"
+      class="p-1 rounded no-outline input"
       :type="type"
       :placeholder="placeholder"
       v-bind="$attrs"
@@ -36,6 +36,15 @@ export default {
 
 <style lang="scss" scoped>
 input {
-  color: var(--text-color-gray-900)
+  color: var(--text-color-gray-900);
+  outline : none;
+  transition: all 0.1s;
+  --shadow-color: #9ca3af ;
+  box-shadow: 6px 6px 0px 1px var(--shadow-color);
+}
+
+.input:focus {
+  transform: translate(2px, 2px);
+  box-shadow: 4px 4px 0px 1px var(--shadow-color);
 }
 </style>
