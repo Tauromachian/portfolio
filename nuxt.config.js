e220245dcd29fde92156b932d4c96a47799a32b9
Jose Garcia <jogarcia@tuta.io>
2021-07-23 16:36:37 -0400

    Add meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,27 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { name: 'author', content: 'jogarcia' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'This is a portfolio website. Where I talk a little about my skills and web presence. If you are looking for developers take a look.'
+      },
+      { name: 'keywords', content: 'portfolio, jogarcia, web, web development, developer, development, backend, frontend, database' },
+      { property: 'og:title', content: 'Portfolio' },
+      { property: 'og:site_name', content: 'Portfolio' },
+      { property: 'og:description', content: 'This is a portfolio website. Where I talk a little about my skills and web presence. If you are looking for developers take a look.' },
+      { property: 'og:keywords', content: 'portfolio, jogarcia, web, web development, developer, development, backend, frontend, database' },
+      { property: 'og:author', content: 'jogarcia' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://nuxt-portfolio-jogarcia.netlify.app/' },
+      { property: 'og:image', content: process.env.BASE_URL + 'portfolio.png' },
+      { property: 'og:image:size', content: '300' },
+      { property: 'og:locale', content: 'en_US' },
+      { property: 'og:locale:alternate', content: 'es_ES' },
+      { property: 'og:copyright', content: 'Portfolio jogarcia' }
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -54,7 +74,9 @@ export default {
   modules: [
     [
       'nuxt-i18n',
-      { /* module options */ }
+      {
+        /* module options */
+      }
     ]
   ],
 
