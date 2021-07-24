b83f82a46c73ad29386ed61d37da5482bbea38b0
Jose Garcia <jogarcia@tuta.io>
2021-07-24 11:58:25 -0400

    Add noPadding prop

--- components/CardText.vue
+++ components/CardText.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="py-3 px-2">
+  <div class="py-3 px-2" :class="{ 'no-padding': noPadding }">
     <slot />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CardText'
+  name: 'CardText',
+  props: {
+    noPadding: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.no-padding {
+  padding: 0 !important;
+}
 </style>
