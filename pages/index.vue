11c7da54bf4fa5e4f8aaee16e6dc84afaf26154c
Jose Garcia <jogarcia@tuta.io>
2021-07-24 12:11:05 -0400

    On smaller screens align images center

--- pages/index.vue
+++ pages/index.vue
@@ -54,7 +54,7 @@
       <div class="flex flex-col">
         <base-card class="bg-secondary mx-4">
           <card-text class="flex" no-padding>
-            <span class="mdi mdi-code-tags text-8xl color-primary mr-2" />
+            <span class="mdi self-center md:self-auto mdi-code-tags text-8xl color-primary mr-2" />
             <div class="flex flex-col">
               <h2>Frontend</h2>
               <p>{{ $t("frontendDescription") }}</p>
@@ -64,7 +64,7 @@
         </base-card>
         <base-card class="bg-secondary mx-4 mt-4 md:mt-8">
           <card-text class="flex" no-padding>
-            <span class="mdi mdi-server text-8xl color-primary mr-2" />
+            <span class="mdi self-center md:self-auto mdi-server text-8xl color-primary mr-2" />
             <div class="flex flex-col">
               <h2>Backend</h2>
               <p>{{ $t("backendDescription") }}</p>
@@ -74,7 +74,7 @@
         </base-card>
         <base-card class="bg-secondary mx-4 mt-4 md:mt-8">
           <card-text class="flex" no-padding>
-            <span class="mdi mdi-database text-8xl color-primary mr-2" />
+            <span class="mdi self-center md:self-auto mdi-database text-8xl color-primary mr-2" />
             <div class="flex flex-col">
               <h2>Database</h2>
               <p>{{ $t("databaseDescription") }}</p>
