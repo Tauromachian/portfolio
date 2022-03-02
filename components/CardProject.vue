40babbfbc6dbe06ea6d400c6ccced109300c0627
Jose Garcia <jogarcia@tuta.io>
2022-03-02 09:37:23 -0500

    Set a max-height for the project card

--- components/CardProject.vue
+++ components/CardProject.vue
@@ -5,7 +5,7 @@
       <h2 class="font-bold">
         {{ title }}
       </h2>
-      <p class="md:h-24 overflow-hidden">
+      <p class="md:h-24 max-h-28 overflow-hidden">
         {{ description }}
       </p>
       <div>
