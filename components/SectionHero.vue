1ae28fd8b7d19b74ba476839b885171ff7ef546e
Tauromachian <betankore@yahoo.com>
2022-03-04 13:57:36 -0500

    Make cards transparent

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -17,7 +17,7 @@
       </p>
       <div class="flex flex-col mt-2">
         <div class="flex flex-col md:flex-row">
-          <base-card class="w-full md:mr-1 py-4">
+          <base-card transparent class="w-full md:mr-1 py-4">
             <card-feature
               :title="$t('intuitive')"
               icon="mdi-lightbulb-on-outline"
@@ -27,7 +27,7 @@
               </p>
             </card-feature>
           </base-card>
-          <base-card class="w-full md:mx-1 py-4">
+          <base-card transparent class="w-full md:mx-1 py-4">
             <card-feature :title="$t('fast')" icon="mdi-rocket-launch">
               <p class="md:text-center md:mx-3">
                 {{ $t("fastText") }}
@@ -36,14 +36,14 @@
           </base-card>
         </div>
         <div class="flex flex-col md:flex-row md:mt-2">
-          <base-card class="w-full md:mr-1 py-4">
+          <base-card transparent class="w-full md:mr-1 py-4">
             <card-feature :title="$t('responsive')" icon="mdi-responsive">
               <p class="md:text-center md:mx-3">
                 {{ $t("responsiveText") }}
               </p>
             </card-feature>
           </base-card>
-          <base-card class="w-full md:mx-1 py-4">
+          <base-card transparent class="w-full md:mx-1 py-4">
             <card-feature :title="$t('reactive')" icon="mdi-alpha-r-circle">
               <p class="md:text-center md:mx-3">
                 {{ $t("reactiveText") }}
