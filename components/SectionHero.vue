0271d5139c89d911a366d0856f7e6bb6515bc942
Tauromachian <betankore@yahoo.com>
2022-03-04 13:39:21 -0500

    Set spacing and size

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,50 +1,55 @@
 <template>
   <section class="flex flex-col md:flex-row mb-32 p-3">
-    <img class="mt-12 rounded-full mx-auto" src="picture-man.jpg" alt="" width="384" height="512">
+    <div class="basis-1/3 h-full">
+      <img
+        class="rounded-full h-full"
+        src="picture-man.jpg"
+        alt="A picture of the honor of this portfolio"
+      >
+    </div>
 
     <div class="flex flex-col justify-center">
-      <h1 class="my-3 md:mt-0 text-3xl text-5xl self-start" :class="titleAlignment">
+      <h1 class="my-3 md:mt-0 text-3xl text-5xl self-start">
         About
       </h1>
       <p>
         {{ $t("aboutText") }}
       </p>
-      <div class="flex flex-col md:flex-row">
-        <div class="flex flex-col">
-          <card-feature
-            :title="$t('intuitive')"
-            icon="mdi-lightbulb-on-outline"
-            class="mt-7"
-          >
-            <p class="md:text-center md:mx-3">
-              {{ $t("intuitiveText") }}
-            </p>
-          </card-feature>
-          <card-feature :title="$t('fast')" icon="mdi-rocket-launch" class="mt-7">
-            <p class="md:text-center md:mx-3">
-              {{ $t("fastText") }}
-            </p>
-          </card-feature>
+      <div class="flex flex-col mt-2">
+        <div class="flex flex-col md:flex-row">
+          <base-card class="w-full md:mr-1 py-4">
+            <card-feature
+              :title="$t('intuitive')"
+              icon="mdi-lightbulb-on-outline"
+            >
+              <p class="md:text-center md:mx-3">
+                {{ $t("intuitiveText") }}
+              </p>
+            </card-feature>
+          </base-card>
+          <base-card class="w-full md:mx-1 py-4">
+            <card-feature :title="$t('fast')" icon="mdi-rocket-launch">
+              <p class="md:text-center md:mx-3">
+                {{ $t("fastText") }}
+              </p>
+            </card-feature>
+          </base-card>
         </div>
-        <div class="flex flex-col">
-          <card-feature
-            :title="$t('responsive')"
-            icon="mdi-responsive"
-            class="mt-7"
-          >
-            <p class="md:text-center md:mx-3">
-              {{ $t("responsiveText") }}
-            </p>
-          </card-feature>
-          <card-feature
-            :title="$t('reactive')"
-            icon="mdi-alpha-r-circle"
-            class="mt-7"
-          >
-            <p class="md:text-center md:mx-3">
-              {{ $t("reactiveText") }}
-            </p>
-          </card-feature>
+        <div class="flex flex-col md:flex-row md:mt-2">
+          <base-card class="w-full md:mr-1 py-4">
+            <card-feature :title="$t('responsive')" icon="mdi-responsive">
+              <p class="md:text-center md:mx-3">
+                {{ $t("responsiveText") }}
+              </p>
+            </card-feature>
+          </base-card>
+          <base-card class="w-full md:mx-1 py-4">
+            <card-feature :title="$t('reactive')" icon="mdi-alpha-r-circle">
+              <p class="md:text-center md:mx-3">
+                {{ $t("reactiveText") }}
+              </p>
+            </card-feature>
+          </base-card>
         </div>
       </div>
     </div>
@@ -57,6 +62,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+@media screen and (min-width: 768px) {
+  section {
+    height: calc(100vh - 64px);
+  }
+}
 
+.basis-1\/3 {
+  flex-basis: 40%;
+}
 </style>
