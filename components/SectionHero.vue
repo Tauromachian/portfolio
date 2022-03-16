ae6930ef25a4dec86b6c1fb2698865df0b603f27
Jose Garcia <jogarcia@tuta.io>
2022-03-16 17:42:49 -0400

    Move about out of hero

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,57 +1,11 @@
 <template>
   <section class="flex flex-col md:flex-row mb-32 p-3">
-    <div class="flex flex-col basis-2/5 h-full justify-center align-center">
+    <div class="flex flex-col basis-2/5 h-full justify-center align-center hero--img-container">
       <img
-        class="rounded-full w-100% h-auto sm:h-96 md:h-auto mx-auto"
+        class="rounded-full w-auto h-auto mx-auto hero--img"
         src="picture-man-min.jpg"
         alt="A picture of the owner of this portfolio"
       >
-    </div>
-
-    <div class="flex flex-col justify-center lg:ml-10">
-      <h1 class="my-3 md:mt-0 text-3xl text-5xl self-start">
-        {{ $t("about") }}
-      </h1>
-      <p>
-        {{ $t("aboutText") }}
-      </p>
-      <div class="flex flex-col mt-2">
-        <div class="flex flex-col md:flex-row">
-          <base-card transparent class="w-full md:mr-1 py-4">
-            <card-feature
-              :title="$t('intuitive')"
-              icon="mdi-lightbulb-on-outline"
-            >
-              <p class="md:text-center md:mx-3">
-                {{ $t("intuitiveText") }}
-              </p>
-            </card-feature>
-          </base-card>
-          <base-card transparent class="w-full md:mx-1 py-4">
-            <card-feature :title="$t('fast')" icon="mdi-rocket-launch">
-              <p class="md:text-center md:mx-3">
-                {{ $t("fastText") }}
-              </p>
-            </card-feature>
-          </base-card>
-        </div>
-        <div class="flex flex-col md:flex-row md:mt-2">
-          <base-card transparent class="w-full md:mr-1 py-4">
-            <card-feature :title="$t('responsive')" icon="mdi-responsive">
-              <p class="md:text-center md:mx-3">
-                {{ $t("responsiveText") }}
-              </p>
-            </card-feature>
-          </base-card>
-          <base-card transparent class="w-full md:mx-1 py-4">
-            <card-feature :title="$t('reactive')" icon="mdi-alpha-r-circle">
-              <p class="md:text-center md:mx-3">
-                {{ $t("reactiveText") }}
-              </p>
-            </card-feature>
-          </base-card>
-        </div>
-      </div>
     </div>
   </section>
 </template>
@@ -70,5 +24,9 @@ export default {
   .basis-2\/5 {
     flex-basis: 40%;
   }
+}
+
+.hero--img-container {
+  width: 50%;
 }
 </style>
