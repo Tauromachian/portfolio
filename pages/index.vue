97bded79069e8e57a7ff1ac2a4a9075581481638
Jose Garcia <jogarcia@tuta.io>
2022-04-05 11:14:43 -0400

    Separate projects section

--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,7 @@
 
     <section-skills />
 
-    <!-- Projects -->
-    <base-section
-      class="flex flex-col items-center justify-center"
-      :title="$t('projects.title')"
-    >
-      <base-card class="w-full">
-        <card-text>
-          <project-carousel />
-        </card-text>
-      </base-card>
-    </base-section>
+    <section-projects />
 
     <!-- Contacts -->
     <base-section
