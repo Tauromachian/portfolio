f47e59f12158d5d75afa788e082c501ebf34f3a6
Jose Garcia <jogarcia@tuta.io>
2022-04-05 10:37:23 -0400

    Make alert component

---
+++ components/AppAlert.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full bg-indigo-900 text-left py-4 lg:px-4 rounded flex items-center" role="alert">
+    <span class="mdi text-3xl mr-3" :class="{ 'mdi-check': type === 'success', 'mdi-alert': type === 'error' }" />
+    <span>
+
+      {{ text }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppAlert',
+  props: {
+    type: {
+      type: String,
+      default: 'success'
+    },
+    text: {
+      type: String,
+      default: 'Success!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
