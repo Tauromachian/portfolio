67641215da429180126a60a26052149db961f98d
Jose Garcia <jogarcia@tuta.io>
2022-04-05 10:39:14 -0400

    Change default color

--- components/AppAlert.vue
+++ components/AppAlert.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full bg-indigo-900 text-left py-4 lg:px-4 rounded flex items-center" role="alert">
+  <div class="w-full bg-primary text-left py-4 lg:px-4 rounded flex items-center" role="alert">
     <span class="mdi text-3xl mr-3" :class="{ 'mdi-check': type === 'success', 'mdi-alert': type === 'error' }" />
     <span>
 
