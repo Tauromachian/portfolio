8d32059db6fabc9cdbd72c89b79904f2d11270e4
Jose Garcia <jogarcia@tuta.io>
2022-04-05 11:13:12 -0400

    Separate skills section

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,10 @@
 <template>
   <div class="container mx-auto">
-    <!-- Hero -->
     <section-hero />
 
     <section-about />
 
-    <!-- Portfolio -->
-    <base-section
-      id="portfolio"
-      class="items-center justify-center"
-      :title="$t('portfolio')"
-    >
-      <div class="flex flex-col">
-        <base-card class="bg-secondary mx-4">
-          <card-text class="flex" no-padding>
-            <span
-              class="mdi self-center md:self-auto mdi-code-tags text-8xl color-primary mr-2"
-            />
-            <div class="flex flex-col">
-              <h2>Frontend</h2>
-              <p>{{ $t("frontendDescription") }}</p>
-              <chip-river :values="frontendTechnologies" class="mt-2" />
-            </div>
-          </card-text>
-        </base-card>
-        <base-card class="bg-secondary mx-4 mt-4 md:mt-8">
-          <card-text class="flex" no-padding>
-            <span
-              class="mdi self-center md:self-auto mdi-server text-8xl color-primary mr-2"
-            />
-            <div class="flex flex-col">
-              <h2>Backend</h2>
-              <p>{{ $t("backendDescription") }}</p>
-              <chip-river :values="backendTechnologies" class="mt-2" />
-            </div>
-          </card-text>
-        </base-card>
-        <base-card class="bg-secondary mx-4 mt-4 md:mt-8">
-          <card-text class="flex" no-padding>
-            <span
-              class="mdi self-center md:self-auto mdi-database text-8xl color-primary mr-2"
-            />
-            <div class="flex flex-col">
-              <h2>{{ $t("database") }}</h2>
-              <p>{{ $t("databaseDescription") }}</p>
-              <chip-river :values="databaseTechnologies" class="mt-2" />
-            </div>
-          </card-text>
-        </base-card>
-        <base-card class="bg-secondary mx-4 mt-4 md:mt-8">
-          <card-text class="flex" no-padding>
-            <span
-              class="mdi self-center md:self-auto mdi-database text-8xl color-primary mr-2"
-            />
-            <div class="flex flex-col">
-              <h2>{{ $t("language") }}</h2>
-              <p>{{ $t("languagesDescription") }}</p>
-              <chip-river :values="languages" class="mt-2" />
-            </div>
-          </card-text>
-        </base-card>
-      </div>
-    </base-section>
+    <section-skills />
 
     <!-- Projects -->
     <base-section
@@ -219,27 +162,6 @@ export default {
       },
       message: { success: true, text: '', active: false },
       loading: false,
-      frontendTechnologies: [
-        'HTML5',
-        'CSS3',
-        'SCSS',
-        'Tailwind CSS',
-        'Bootstrap',
-        'Vuetify',
-        'Javascript',
-        'Vue.js',
-        'Angular',
-        'Nuxt.js'
-      ],
-      backendTechnologies: [
-        'Javascript',
-        'PHP',
-        'Node.js',
-        'Express.js',
-        'Laravel'
-      ],
-      databaseTechnologies: ['MariaDB', 'MySQL', 'PostgreSQL', 'MongoDB'],
-      languages: ['English', 'Español'],
       socialLinks: [
         {
           link: 'https://www.linkedin.com/in/jose-garcia-888941180/',
