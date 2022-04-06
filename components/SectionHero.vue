0bb17b41f4f89eca0d9a6c32c1a4ad44ef810aae
Jose Garcia <jogarcia@tuta.io>
2022-04-06 11:20:53 -0400

    Remake hero section

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,17 +1,24 @@
 <template>
   <section class="flex flex-col md:flex-row mb-32 p-3 relative">
-    <div class="flex flex-col h-full justify-center">
+    <div class="flex flex-col h-full justify-center hero__text">
       <h1 class="text-7xl md:text-8xl">
         Making ideas happen
       </h1>
-      <p>Web developer/ Fullstacker</p>
-      <base-button>I want to check your skills</base-button>
+      <p class="mt-5">
+        <strong>
+          Web developer/ Fullstacker
+        </strong>
+      </p>
+      <div>
+        <base-button class="mt-5">
+          I want to check your skills
+        </base-button>
+      </div>
     </div>
-    <div class="flex flex-col h-full justify-center">
-      <pre>
-        <pre><code class="language-html" v-text="content" /></pre>
-      </pre>
+    <div class="flex flex-col h-full justify-center hero__code">
+      <pre class="line-numbers"><code class="language-html" v-text="content" /></pre>
     </div>
+    <span class="mdi mdi-arrow-down scroll-icon" />
   </section>
 </template>
 
@@ -22,7 +29,7 @@ export default {
   name: 'SectionHero',
   computed: {
     content () {
-      return '<div>\n\t<h1>Making ideas happen</h1>\n\t<p>Web developer/ Fullstacker</p>\n\t<button>I want to check your skills</button>\n</div>'
+      return '<section>\n\t<h1 class="font-size-large">Making ideas happen</h1>\n\t<p class="margin-top-5">\n\t\t<strong>Web developer/ Fullstacker</strong>\n\t</p>\n\t<button>I want to check your skills</button>\n</section>'
     }
   },
   mounted () {
@@ -37,16 +44,13 @@ export default {
   section {
     height: calc(100vh - 64px);
   }
-  .basis-2\/5 {
-    flex-basis: 40%;
-  }
 }
 
-.hero--img-container {
+.hero__text {
   flex-basis: 50%;
 }
 
-.hero--information-container {
+.hero__code {
   flex-basis: 50%;
 }
 
@@ -56,6 +60,13 @@ export default {
 
 .scroll-icon {
   animation: up-and-down 1s infinite ease-out;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 2rem;
 }
 
 @keyframes up-and-down {
