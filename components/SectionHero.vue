0070e07fba6016d6d41a72a91dbafea95e437276
Jose Garcia <jogarcia@tuta.io>
2022-04-06 16:54:12 -0400

    Set layout for intermediary sizes

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="flex flex-col md:flex-row mb-32 p-3 relative">
-    <div class="flex flex-col h-full justify-center hero__text">
-      <h1 class="text-7xl md:text-8xl">
+  <section class="flex flex-col lg:flex-row mb-32 p-3 relative">
+    <div class="flex flex-col h-full justify-center hero__text md:mr-5 lg:mr-0">
+      <h1 class="text-7xl xl:text-8xl">
         Making ideas happen
       </h1>
       <p class="mt-10 text-xl">
@@ -14,7 +14,7 @@
           I will build your site and make it look awesome
         </strong>
       </p>
-      <div class="flex flex-col md:flex-row">
+      <div class="flex flex-col sm:flex-row lg:flex-col xl:flex-row">
         <a href="#portfolio">
           <base-button class="mt-12 relative w-full md:w-unset" is-call-to-action>
             <div class="w-full h-full" />
@@ -24,7 +24,7 @@
           </base-button>
         </a>
         <a href="#social-networks">
-          <base-button class="mt-2 md:mt-12 md:ml-2 relative w-full md:w-unset" is-call-to-action outline>
+          <base-button class="mt-2 sm:mt-12 lg:mt-2 xl:mt-12 sm:ml-2 lg:ml-0 xl:ml-2 relative w-full md:w-unset" is-call-to-action outline>
             <div class="w-full h-full" />
             <span class="mdi mdi-email absolute left-5" />
             <span class="mx-2" />
@@ -33,7 +33,7 @@
         </a>
       </div>
     </div>
-    <div class="flex flex-col h-full justify-center hero__code">
+    <div class="flex flex-col h-full justify-center hero__code md:max-w-lg lg:max-w-unset">
       <pre class="line-numbers code-handler-styles" data-src="/markup.html" />
     </div>
     <span class="mdi mdi-arrow-down scroll-icon hidden md:visible" />
