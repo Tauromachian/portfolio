c3f1af7002ef036177da8302f569722b0a6ed58d
Jose Garcia <jogarcia@tuta.io>
2022-04-06 16:56:55 -0400

    Set max-w-max

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -33,7 +33,7 @@
         </a>
       </div>
     </div>
-    <div class="flex flex-col h-full justify-center hero__code md:max-w-lg lg:max-w-unset">
+    <div class="flex flex-col h-full justify-center hero__code md:max-w-lg lg:max-w-max">
       <pre class="line-numbers code-handler-styles" data-src="/markup.html" />
     </div>
     <span class="mdi mdi-arrow-down scroll-icon hidden md:visible" />
