44637da14a0234fc354329120d0703dcb4f8e825
Jose Garcia <jogarcia@tuta.io>
2022-08-08 17:19:18 -0400

    Change top navbar style

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,24 +1,36 @@
 <template>
   <nav class="flex bg-primary">
-    <div class="container flex text-2xl ml-14 z-50">
-      <ul class="flex items-center text-xl ">
-        <li v-for="link in links" :key="link.link + link.text" class="px-3 navbar__link">
+    <div class="container flex justify-center text-2xl ml-14 z-50">
+      <div class="flex items-center">
+        <h4 class="text-3xl">
+          José García
+        </h4>
+      </div>
+
+      <ul class="flex items-center text-xl mx-auto">
+        <li
+          v-for="link in links"
+          :key="link.link + link.text"
+          class="px-3 navbar__link"
+        >
           <a :href="link.link" @click.prevent="scrollToPosition(link.link)">
             {{ $t(link.text) }}
           </a>
         </li>
       </ul>
-      <base-button class="navbar__link" icon @click="changeLocal">
-        <span class="mdi mdi-translate" />
-      </base-button>
-      <base-menu bottom="initial" top="3rem">
-        <template #activator="{ on }">
-          <base-button class="navbar__link" icon v-on="on">
-            <span class="mdi mdi-invert-colors" />
-          </base-button>
-        </template>
-        <theme-selector v-model="theme" :themes="themes" />
-      </base-menu>
+      <div>
+        <base-button class="navbar__link" icon @click="changeLocal">
+          <span class="mdi mdi-translate" />
+        </base-button>
+        <base-menu bottom="initial" top="3rem">
+          <template #activator="{ on }">
+            <base-button class="navbar__link" icon v-on="on">
+              <span class="mdi mdi-invert-colors" />
+            </base-button>
+          </template>
+          <theme-selector v-model="theme" :themes="themes" />
+        </base-menu>
+      </div>
     </div>
   </nav>
 </template>
