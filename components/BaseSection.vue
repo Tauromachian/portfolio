d07a7b10585e3eca5a1b76e3cc12e55ebc32e7dd
Jose Garcia <jogarcia@tuta.io>
2022-08-08 20:25:49 -0400

    Set default header to h2

--- components/BaseSection.vue
+++ components/BaseSection.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="flex flex-col my-32 p-3">
-    <h1 class="mb-3 text-3xl text-5xl self-start sm:self-center" :class="titleAlignment">
+    <h2 class="mb-3 text-3xl text-5xl self-start sm:self-center" :class="titleAlignment">
       {{ title }}
-    </h1>
+    </h2>
     <slot />
   </section>
 </template>
