dcb84107ca45c7357f66169f5d56f6103bd6150a
Jose Garcia <jogarcia@tuta.io>
2022-08-09 12:08:27 -0400

    Use new image format providing fallback

--- components/CardProject.vue
+++ components/CardProject.vue
@@ -1,14 +1,17 @@
 <template>
   <base-card class="rounded-lg">
-    <img
-      loading="lazy"
-      decoding="async"
-      :src="`${src}.jpg`"
-      width="1908"
-      height="930"
-      :alt="alt"
-      class="max-w-full h-auto align-middle border-none rounded-t-lg"
-    >
+    <picture>
+      <source type="image/avif" :srcset="`${src}.avif`">
+      <img
+        loading="lazy"
+        decoding="async"
+        :src="`${src}.jpg`"
+        width="1908"
+        height="930"
+        :alt="alt"
+        class="max-w-full h-auto align-middle border-none rounded-t-lg"
+      >
+    </picture>
     <card-text class="flex flex-col px-5 mt-3">
       <h2 class="font-bold">
         {{ title }}
