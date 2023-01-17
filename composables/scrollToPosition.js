1032d8ea050799c783777a21ed77b7e320f3ad3c
Jose Garcia <jogarcia@tuta.io>
2023-01-17 17:02:24 -0500

    Fix parameter typo

--- composables/scrollToPosition.js
+++ composables/scrollToPosition.js
@@ -1,4 +1,4 @@
-export const scrollToPosition = (postitionId) => {
-  const elmnt = document.getElementById(postitionId.split("#").pop());
+export const scrollToPosition = (positionId) => {
+  const elmnt = document.getElementById(positionId.split("#").pop());
   elmnt.scrollIntoView({ behavior: "smooth" });
 };
