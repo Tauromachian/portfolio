8bc24d56c37fb5afd411671251ca000ef2a311c2
Jose Garcia <jogarcia@tuta.io>
2023-01-17 16:20:10 -0500

    Ref/ Improve organization

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,7 +1,7 @@
+import { createI18n } from "vue-i18n";
+
 import en from "@/locales/en.json";
 import es from "@/locales/es.json";
-
-import { createI18n } from "vue-i18n";
 
 export default defineNuxtPlugin(({ vueApp }) => {
   const i18n = createI18n({
