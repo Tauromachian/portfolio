e263de5d22dbfe486c746ffd6fa43814526c0946
Jose Garcia <jogarcia@tuta.io>
2023-01-20 09:15:58 -0500

    Add more spacing on smaller screens

--- components/SectionAbout.vue
+++ components/SectionAbout.vue
@@ -34,7 +34,7 @@
             "
           />
           <img
-            class="w-full h-auto rounded-full mt-5 lg:mt-0"
+            class="w-full h-auto rounded-full mt-10 lg:mt-0"
             decoding="async"
             loading="lazy"
             width="1256"
@@ -48,7 +48,7 @@
         </picture>
       </div>
     </div>
-    <div class="flex flex-col justify-center sm:flex-row mt-32">
+    <div class="flex flex-col justify-center sm:flex-row mt-40 md:mt-32">
       <div class="flex flex-col md:flex-row">
         <base-card transparent class="w-full md:mr-1 py-4">
           <card-feature
