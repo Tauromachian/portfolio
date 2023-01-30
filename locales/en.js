c57d83e930bd72ba543edf84dace8d96e2431fb5
Jose Garcia <jogarcia@tuta.io>
2023-01-30 11:29:22 -0500

    Use js instead of json

---
+++ locales/en.js
@@ -0,0 +1,65 @@
+export default {
+  hero: {
+    title: "Making ideas happen",
+    description1: "I am a a web developer/fullstacker",
+    description2: " I will build your site and make it look awesome",
+    button1: "Check skills",
+    button2: "Contact me",
+  },
+  home: "Home",
+  visit: "Visit",
+  about: "About",
+  aboutText1:
+    "Hello there! My name is José García. I am a fullstack web developer. I enjoy making ideas happen on the web using current development technologies. I build clean, well-designed and user-friendly (with SEO in mind) sites and apps.",
+  aboutText2:
+    "I have been working in the field for over 3 years now, helping small businesses grow and thrive by designing custom websites that help them to communicate better with their customers and increase revenue through better visibility on search engines such as Google, Bing and Yahoo!",
+  aboutText3:
+    "My clients range from one person micro businesses to large companies like Dt4o . Let's discuss your needs and goals so I can provide you with a plan that will get you the results you want.",
+  myPictureAlt: "My personal picture",
+  intuitive: "Intuitive",
+  intuitiveText: "User experience focused web sites",
+  fast: "Fast",
+  fastText: "The site's performance is a priority",
+  responsive: "Responsive",
+  responsiveText: "Web sites that work and look good in different screens",
+  reactive: "Reactive",
+  reactiveText: "Instant reactions to the user's interactions",
+  skills: "Skills",
+  contacts: "Contacts",
+  repositories: "Repositories",
+  formTitle: "Have a question or want to work together?",
+  testing: "Testing",
+  projects: {
+    title: "Projects",
+    description: {
+      queues:
+        "This project is about taking tickets to for virtual queues. It aims to eliminate physical queues.",
+      "johnson-turbine":
+        "For over 25 years, Johnson Turbines Support have supported the GG4/FT4 market with an extensive inventory, knowledgeable customer service, and fast and reliable shipping.",
+      kanpeki:
+        "Santiaguero project that covers Asian and Western pop culture (includes Japanese, Korean, Chinese pop and traditional culture, etc.; in addition to video games, comics, literature in general, cinephilia and world-class music).",
+      sanchoRestaurant:
+        "Sancho Restaurant, delicious cuisine specialties created with ingenuity and passion, inspired by the fantastic flavors of Spain.",
+      zaruArquitectura:
+        "Design of different homes and custom exterior and interior decorations.",
+      flowers:
+        "Sell flowers in their flowerpots so the clients can take care of them. Make the world a beautiful place.",
+    },
+  },
+  form: {
+    name: "Name",
+    email: "Email",
+    message: "Message",
+  },
+  button: {
+    send: "Send",
+  },
+  frontendDescription: "The visuals the beauty of the project.",
+  backendDescription:
+    "Part of the app the user doesn't see. The brains of the project.",
+  database: "Database",
+  databaseDescription: "They way you save data.",
+  testingDescription: "Make sure everything works as it should.",
+  language: "Languages",
+  languagesDescription: "The languages in which you can communicate with me",
+};
