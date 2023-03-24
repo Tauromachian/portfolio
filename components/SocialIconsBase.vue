94345ccfb8ce1220af7d9622a2dc2e3eb7fcd12c
Jose Garcia <jogarcia@tuta.io>
2023-03-24 09:51:43 -0400

    Add fill current color

--- components/SocialIconsBase.vue
+++ components/SocialIconsBase.vue
@@ -1,6 +1,6 @@
 <template>
   <svg
-    :fill="color"
+    fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
     :viewBox="icon == 'telegram' ? '0 0 32 32' : '0 0 24 24'"
     :width="width"
