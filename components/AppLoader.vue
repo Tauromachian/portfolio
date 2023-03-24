5a8b066c068d1276b522e86686d4a865f7a5ab5c
Jose Garcia <jogarcia@tuta.io>
2023-03-23 21:02:24 -0400

    Make AppLoader component

---
+++ components/AppLoader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="spinner-border animate-spin inline-block w-6 h-6 border-4 border-top-primary rounded-full"
+    role="status"
+  />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
