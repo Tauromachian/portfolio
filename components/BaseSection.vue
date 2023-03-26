d69ae6ae57457e932c9d81cc49f1530de22b6c4e
Jose Garcia <jogarcia@tuta.io>
2023-03-26 18:40:25 -0400

    Make sure there are no "no text" titles

--- components/BaseSection.vue
+++ components/BaseSection.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="flex flex-col my-32 p-3">
     <h2
+      v-if="title"
       class="mb-3 text-3xl text-5xl self-start sm:self-center"
       :class="titleAlignment"
     >
@@ -12,18 +13,18 @@
 
 <script>
 export default {
-  name: 'BaseSection',
+  name: "BaseSection",
   props: {
     title: {
       type: String,
-      default: ''
+      default: "",
     },
     titleAlignment: {
       type: [String],
-      default: ''
-    }
-  }
-}
+      default: "",
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
