0d0dde9bebfaed8c5d24154a3e9a6ba1b524cb4f
Jose Garcia <betankore@gmail.com>
2023-08-26 11:21:00 -0400

    Remove unnecessary component

--- components/BaseBall.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="rounded-full absolute ball" v-bind="$attrs"></div>
-</template>
-
-<style lang="scss" scoped>
-.ball {
-  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
-}
-</style>
