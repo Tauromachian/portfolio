b2ff2b23fc057e51aa43f140efd10c5fcc85caf6
Tauromachian <54786476+Tauromachian@users.noreply.github.com>
2023-08-26 15:06:11 +0000

    Build: (ac2d26a) Remove unnecessary data prop

--- _nuxt/i18n.config.30ad636d.js
+++
@@ -1,1 +0,0 @@
-import{G as e,H as s}from"./entry.205dd76b.js";const o=()=>({legacy:!1,locale:"en",messages:{es:e,en:s}});export{o as default};
--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1693062296996}
+export default {data:{},prerenderedAt:1693062368580}
